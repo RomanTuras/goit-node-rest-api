d1310345e1a7eee4051c1baf3734f70ec9cf9d55
RomanTuras <salden.com@gmail.com>
2025-06-22 13:42:21 +0300

    Remove username from schema

--- schemas/authSchemas.js
+++ schemas/authSchemas.js
@@ -3,7 +3,6 @@ import Joi from "joi";
 import { emailRegexp } from "../constants/auth.js";
 
 export const authRegisterSchema = Joi.object({
-    username: Joi.string().required(),
     email: Joi.string().pattern(emailRegexp).error(new Error('Помилка від Joi або іншої бібліотеки валідації')).required(),
     password: Joi.string().min(6).required(),
 });
